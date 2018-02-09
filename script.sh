5bc7124d4c599f744a4eb18d8c68ee048572a494
Darren Worrall <darren.worrall@shopify.com>
2018-02-09 11:29:54 -0500

    Optional sleep

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "${SLEEP:-0}" -gt 0 ]]; then
+    echo "Sleeping for ${SLEEP} seconds..."
+    sleep ${SLEEP}
+fi
+
 if [[ "$BUILDKITE_RETRY_COUNT" -gt 0 ]]; then
     echo "Success!"
     exit 0
